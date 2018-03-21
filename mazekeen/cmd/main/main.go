3615db4d811da623a9d1169e8f19d5ca9e29349f
Leonardo Di Donato <leodidonato@gmail.com>
2018-03-21 20:01:22 +0100

    Update: Mazekeen

--- mazekeen/cmd/main/main.go
+++ mazekeen/cmd/main/main.go
@@ -7,9 +7,11 @@ import (
 	"time"
 
 	"github.com/leodido/go-monorepo/lucifer"
+	uuid "github.com/satori/go.uuid"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	fmt.Fprintf(os.Stdout, "Mazekeen works for %s\n", lucifer.GetName())
+	fmt.Fprintf(os.Stdout, "And always will, regardless the UUID: %s.\n", uuid.NewV4())
 }
