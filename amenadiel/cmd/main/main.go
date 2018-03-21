726cd6ab47cf9147c77e1c03c86d7b71dd211095
Leonardo Di Donato <leodidonato@gmail.com>
2018-03-21 20:01:10 +0100

    Update: Amenadiel

--- amenadiel/cmd/main/main.go
+++ amenadiel/cmd/main/main.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/leodido/go-monorepo/amenadiel"
 	"github.com/leodido/go-monorepo/lucifer"
+	uuid "github.com/satori/go.uuid"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	fmt.Fprintf(os.Stdout, "%s is an %s\n", amenadiel.Amenadiel.Name, amenadiel.Amenadiel.Level)
 	fmt.Fprintf(os.Stdout, "He tries to take the %s back to its home - ie., the %s\n", lucifer.GetName(), lucifer.Home())
+	fmt.Fprintf(os.Stdout, "Regardless the UUID: %s.\n", uuid.Must(uuid.NewV4()))
 }
