2393bc30d0c18ab087482b87305422f5c1bdde52
Leonardo Di Donato <leodidonato@gmail.com>
2018-03-21 19:20:24 +0100

    Update: Amenadiel message

--- amenadiel/cmd/main/main.go
+++ amenadiel/cmd/main/main.go
@@ -5,8 +5,10 @@ import (
 	"os"
 
 	"github.com/leodido/go-monorepo/amenadiel"
+	"github.com/leodido/go-monorepo/lucifer"
 )
 
 func main() {
 	fmt.Fprintf(os.Stdout, "%s is an %s", amenadiel.Amenadiel.Name, amenadiel.Amenadiel.Level)
+	fmt.Fprintf(os.Stdout, "He tries to take the %s back to its home - ie., the %s", lucifer.GetName(), lucifer.Home())
 }
